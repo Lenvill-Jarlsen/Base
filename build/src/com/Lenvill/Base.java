e3664bcf7c3fb178e8720123689dd90654102e7d
Lenvill-Jarlsen <118316016+Lenvill-Jarlsen@users.noreply.github.com>
2024-07-11 22:36:43 -0400

    Update 1.1
    Fixed broken dev environment and removed dependencies that weren't supposed to be in the base file

--- build/src/com/Lenvill/Base.java
+++ build/src/com/Lenvill/Base.java
@@ -13,7 +13,7 @@ public class Base {
     public static final String MODID = "base";
     public static final String NAME = "Base";
     public static final String VERSION = "1.12.2-1.0";
-    public static final String DEPENDENCIES = "required-after:baubles;" + "required-after:embers;" + "required-after:minefantasyreforged;";
+    public static final String DEPENDENCIES = "required-after:minefantasyreforged;";
 
     @SidedProxy(clientSide = "com.Lenvill.proxy.ClientProxy", serverSide = "com.Lenvill.proxy.CommonProxy")
     public static CommonProxy PROXY;
