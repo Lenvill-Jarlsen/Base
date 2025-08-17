03f63ed930c4517eb81894ad90e3fd231193d18d
Lenvill-Jarlsen <118316016+Lenvill-Jarlsen@users.noreply.github.com>
2025-08-16 23:55:58 -0400

    Initial Development

--- src/main/java/com/Lenvill/proxy/CommonProxy.java
+++ src/main/java/com/Lenvill/proxy/CommonProxy.java
@@ -1,6 +1,12 @@
 package com.Lenvill.proxy;
 
+import net.minecraft.item.Item;
+
 public class CommonProxy {
+
+    public void registerItemRenderer(Item item, int meta, String id){
+        //NOOP for CommonProxy
+    }
 
     public void preInit() {
         //NOOP for commonProxy
